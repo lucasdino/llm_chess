16fdf4c8b965e59321c4d4e7c6e403f58a672f36
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-08-05 10:49:55 -0700

    updated to allow for reasoning levels

--- llm_chess/utils/vllm_api.py
+++ llm_chess/utils/vllm_api.py
@@ -11,12 +11,6 @@ class vLLMClient:
         self.base_url = base_url
 
         self.generation_args = generation_args
-        self.max_tokens = generation_args["max_tokens"]
-        self.temperature = generation_args["temperature"]
-        self.top_p = generation_args["top_p"]
-        self.min_p = generation_args["min_p"]
-        self.top_k = generation_args["top_k"]
-        self.repetition_penalty = generation_args["repetition_penalty"]
 
         self.headers = {
             "Content-Type": "application/json",
@@ -27,12 +21,7 @@ class vLLMClient:
         payload = {
             "model": self.model,
             "prompt": prompt,
-            "max_tokens": self.max_tokens,
-            "temperature": self.temperature,
-            "top_p": self.top_p,
-            "top_k": self.top_k,
-            "min_p": self.min_p,
-            "repetition_penalty": self.repetition_penalty
+            **self.generation_args
         }
         try:
             response = requests.post(f"{self.base_url}", json=payload, headers=self.headers)
